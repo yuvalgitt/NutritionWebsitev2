03cfc4e00fdababa9d032b096fbcdf7a7ea12c59
Yuval Dayan <ydayan232@gmail.com>
2024-12-20 10:40:53 +0200

    added intake module

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -2,12 +2,14 @@ import { Module } from '@nestjs/common';
 import { FoodsModule } from './foods/foods.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './users/users.module';
+import { IntakeModule } from './intake/intake.module';
 
 @Module({
   imports: [
     FoodsModule,
     MongooseModule.forRoot('mongodb://127.0.0.1/NutritionProject'),
-    UsersModule
+    UsersModule,
+    IntakeModule
   ],
   controllers: [],
   providers: [],
